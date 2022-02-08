138594c811b02c5d69f8fca94a50388c8b0cbb05
SteeleDynamics <SteeleDynamics@gmail.com>
2022-02-08 17:22:11 -0500

    SICP Chapter 1 (In Progress...)
    1. Completed exercise 1.27

---
+++ ch1/ex1.27/ex1.27.scm
@@ -0,0 +1,20 @@
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))        
+
+(define (fermat-test-all n a)
+  (if (< a n)
+      (and (= (expmod a n n) a) (fermat-test-all n (+ a 1)))
+      true))
+
+(fermat-test-all 561 0)
+(fermat-test-all 1105 0)
+(fermat-test-all 1729 0)
+(fermat-test-all 2465 0)
+(fermat-test-all 2821 0)
+(fermat-test-all 6601 0)
