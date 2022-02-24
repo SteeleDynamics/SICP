d9d969767b8075dd83192dd3be9795cfe2268512
SteeleDynamics <SteeleDynamics@gmail.com>
2022-02-24 10:04:40 -0500

    SICP Chapter 2 (In Progress...)
    1. Renamed reverse, deep-reverse, and fringe to reverse-iter,
       deep-reverse-iter, and fringe-iter.
    2. Added reverse-rec, deep-reverse-rec, and fringe-rec recursive
       processes.
    3. Updated and added unit tests.

--- ch2/ex2.27/ex2.27.scm
+++ ch2/ex2.27/ex2.27.scm
@@ -1,16 +1,22 @@
-; nil def'n
+; nil defn
 (define nil '())
 
-; reverse procedure
-(define (reverse xs)
+; reverse iterative procedure
+(define (reverse-iter xs)
   (define (iter xs acc)
     (if (null? xs)
         acc
         (iter (cdr xs) (cons (car xs) acc))))
   (iter xs nil))
 
-; deep-reverse procedure
-(define (deep-reverse xs)
+; reverse recursive procedure
+(define (reverse-rec xs)
+  (if (null? xs)
+      nil
+      (append (reverse-rec (cdr xs)) (list (car xs)))))
+
+; deep-reverse iterative procedure
+(define (deep-reverse-iter xs)
   (define (iter xs acc)
     (cond ((null? xs) acc)
           ((not (pair? xs)) xs)
@@ -19,6 +25,19 @@
                             acc)))))
   (iter xs nil))
 
-; unit tests for reverse and deep-reverse
-(deep-reverse '((1 2) (3 4)))
-(deep-reverse '(1 2 (3 4) 5 (6 (7 8) 9) 10))
+; deep-reverse recursive procedure
+(define (deep-reverse-rec xs)
+    (cond ((null? xs) nil)
+          ((not (pair? xs)) xs)
+          (else (append (deep-reverse-rec (cdr xs))
+                        (list (deep-reverse-rec (car xs)))))))
+
+; unit tests for reverse-iter and reverse-rec
+(reverse-iter '(1 2 3 4))
+(reverse-rec '(1 2 3 4))
+
+; unit tests for deep-reverse-iter and deep-reverse-rec
+(deep-reverse-iter '((1 2) (3 4)))
+(deep-reverse-iter '(1 2 (3 4) 5 (6 (7 8) 9) 10))
+(deep-reverse-rec '((1 2) (3 4)))
+(deep-reverse-rec '(1 2 (3 4) 5 (6 (7 8) 9) 10))
--- ch2/ex2.28/ex2.28.scm
+++ ch2/ex2.28/ex2.28.scm
@@ -1,8 +1,8 @@
-; nil def'n
+; nil defn
 (define nil '())
 
-; fringe proc
-(define (fringe xs)
+; fringe iterative process
+(define (fringe-iter xs)
   (define (iter xs acc)
     (cond ((null? xs) acc)
           ((not (pair? xs)) (cons xs acc))
@@ -10,7 +10,16 @@
                       (iter (cdr xs) acc)))))
   (iter xs nil))
 
-; fringe unit tests
+; fringe recursive process
+(define (fringe-rec xs)
+  (cond ((null? xs) nil)
+        ((not (pair? xs)) (list xs))
+        (else (append (fringe-rec (car xs))
+                      (fringe-rec (cdr xs))))))
+
+; fringe-iter and fringe-rec unit tests
 (define x (list (list 1 2) (list 3 4)))
-(fringe x)
-(fringe (list x x))
+(fringe-iter x)
+(fringe-iter (list x x))
+(fringe-rec x)
+(fringe-rec (list x x))
