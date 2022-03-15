bf025faced88c05c00058aebd7a9e0aa29b27622
SteeleDynamics <SteeleDynamics@gmail.com>
2022-03-15 11:18:03 -0400

    SICP Chapter 2 (In Progress...)
    1. Completed exercises 2.75 through 2.76
    2. Completed §2.4 'Multiple Representations for Abstract Data'

---
+++ ch2/ex2.75/ex2.75.scm
@@ -0,0 +1,24 @@
+; make-from-real-imag procedure
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude)
+           (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle) (atan y x))
+          (else
+           (error "Unknown op -- MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
+
+; make-from-mag-ang procedure
+(define (make-from-mag-ang r a)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* r (cos a)))
+          ((eq? op 'imag-part) (* r (sin a)))
+          ((eq? op 'magnitude) r)
+          ((eq? op 'angle) a)
+          (else
+           (error "Unknown op -- MAKE-FROM-MAG-ANG" op))))
+  dispatch)
+
+(define (apply-generic op arg) (arg op))
