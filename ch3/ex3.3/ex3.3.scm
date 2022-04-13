9ae2d7d28993f373d9357cc6f95ea8969afb04a0
SteeleDynamics <SteeleDynamics@gmail.com>
2022-04-13 17:03:26 -0400

    SICP Chapter 3 (In Progress...)
    1. Completed exercises 3.3 through 3.4

---
+++ ch3/ex3.3/ex3.3.scm
@@ -0,0 +1,21 @@
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch p m)
+    (cond ((not (eq? p password)) (lambda (x) "Incorrect password"))
+          ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request -- MAKE-ACCOUNT"
+                       m))))
+  dispatch)
+
+; unit tests
+(define acc (make-account 100 'secret-password))
+((acc 'secret-password 'withdraw) 40)
+((acc 'some-other-password 'deposit) 50)
