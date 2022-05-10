fb4e40a2aa7da3cd3bdf4f862e83120d349d8f94
SteeleDynamics <SteeleDynamics@gmail.com>
2022-05-10 16:58:27 -0400

    SICP Chapter 3 (In Progress...)
    1. Completed exercise 3.17 (count-pairs procedure using DFS)

---
+++ ch3/ex3.17/ex3.17.scm
@@ -0,0 +1,35 @@
+; member procedure
+(define (member item x)
+  (cond ((null? x) false)
+        ((equal? item (car x)) x)
+        (else (member item (cdr x)))))
+
+; count-pairs procedure
+(define (count-pairs x)
+  (define visited '())
+  (define (visit-pairs u)
+    (cond ((not (pair? u)) 0)
+          ((member u visited) 0)
+          (else 
+            (set! visited (cons u visited))
+            (+ (visit-pairs (car u))
+               (visit-pairs (cdr u))
+               1))))
+  (visit-pairs x))
+
+; unit tests
+(define x (list 'a 'b 'c))
+(count-pairs x)
+
+(define x (list 'a 'b))
+(define y (cons x (cdr x)))
+(count-pairs y)
+
+(define x (list 'a))
+(define y (cons x x))
+(define z (cons y y))
+(count-pairs z)
+
+(define x (list 'a 'b 'c))
+(set-cdr! (last-pair x) x)
+(count-pairs x)
