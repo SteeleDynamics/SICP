c0eea55d354cedeca93bbdb77c27efc0db999f37
SteeleDynamics <SteeleDynamics@gmail.com>
2022-06-23 19:15:21 -0400

    SICP Chapter 3 (In Progress...)
    1. Completed exercise 3.53 (Sieve of Eratosthenes, stream-take)
       a. Impl'ed the following procedures: stream-take,
          integers-starting-from, divisible?, sieve, add-streams, and
          scale-stream
       b. Impl'ed the following streams: primes, ones, integers, and fibs

---
+++ ch3/ex3.53/ex3.53.scm
@@ -0,0 +1,190 @@
+; memo-proc procedure
+(define (memo-proc proc)
+  (let ((already-run? false) (result false))
+    (lambda ()
+      (if (not already-run?)
+          (begin (set! result (proc))
+                 (set! already-run? true)
+                 result)
+          result))))
+
+#|
+ | §3.5.1 Footnote 56:
+ |
+ | Although stream-car and stream-cdr can be defined as procedures, cons-stream
+ | must be a special form. If cons-stream were a procedure, then, according to
+ | our model of evaluation, evaluating (cons-stream <a> <b>) would automatically
+ | cause <b> to be evaluated, which is precisely what we do not want to happen.
+ | For the same reason, delay must be a special form, though force can be an
+ | ordinary procedure.
+ |#
+
+; delay procedure -- create thunk
+; (define (delay expr) (lambda () expr))
+; (define (delay expr) (memo-proc (lambda () expr)))
+(define-syntax delay
+  (syntax-rules ()
+    ((delay ?expr) (memo-proc (lambda () ?expr)))))
+
+; force procedure -- evaluate thunk
+(define (force delayed-expr) (delayed-expr))
+
+; the-empty-stream defn
+(define the-empty-stream '())
+
+; cons-stream constructor procedure
+; (define (cons-stream a b) (cons a (delay b)))
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((cons-stream a b) (cons a (delay b)))))
+
+; stream-car selector procedure
+(define (stream-car stream) (car stream))
+
+; stream-cdr selector procedure
+(define (stream-cdr stream) (force (cdr stream)))
+
+; stream-null? predicate procedure
+(define (stream-null? stream) (null? stream))
+
+; stream-ref procedure
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+#|
+ | ; stream-map procedure
+ | (define (stream-map proc s)
+ |   (if (stream-null? s)
+ |       the-empty-stream
+ |       (cons-stream (proc (stream-car s))
+ |                    (stream-map proc (stream-cdr s)))))
+ |#
+
+; stream-for-each procedure
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+#|
+ | ; display-stream procedure
+ | (define (display-stream s)
+ |   (stream-for-each display-line s))
+ |
+ | ; display-line procedure
+ | (define (display-line x)
+ |   (newline)
+ |   (display x))
+ |#
+
+; display-stream procedure (instructor's manual, augmented)
+(define display-stream
+  (let ()
+    (define (iter stream delim)
+      (cond ((stream-null? stream) (display "]"))
+            (else (display delim)
+                  (display (stream-car stream))
+                  (iter (stream-cdr stream) " "))))
+    (lambda (stream)
+      (newline)
+      (display "[")
+      (iter stream ""))))
+
+; stream-enumerate-interval procedure
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+; stream-filter procedure
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+; stream-map procedure
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+        (apply proc (map stream-car argstreams))
+        (apply stream-map
+          (cons proc (map stream-cdr argstreams))))))
+
+; stream-take procedure
+(define (stream-take s n)
+  (cond ((< n 0) (error "Subscript"))
+        ((= n 0) '())
+        ((stream-null? s) (error "Subscript"))
+        (else (cons (stream-car s)
+                    (stream-take (stream-cdr s) (- n 1))))))
+
+; integers-starting-from procedure
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+; divisible predicate procedure
+(define (divisible? x y) (= (remainder x y) 0))
+
+; (Sieve of Eratosthenes) sieve procedure
+(define (sieve stream)
+  (cons-stream
+   (stream-car stream)
+   (sieve (stream-filter
+           (lambda (x)
+             (not (divisible? x (stream-car stream))))
+           (stream-cdr stream)))))
+
+; (Sieve of Eratosthenes) primes stream
+(define primes (sieve (integers-starting-from 2)))
+
+; 51st prime number ==> 233
+(stream-ref primes 50)
+
+; list of the first 51 prime numbers
+(stream-take primes 51)
+
+; ones stream
+(define ones (cons-stream 1 ones))
+
+; add-streams procedure
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+; integers stream
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+; fibs stream
+(define fibs
+  (cons-stream 0 (cons-stream 1 (add-streams (stream-cdr fibs) fibs))))
+
+; scale-stream procedure
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+#|
+ | Exercise 3.53
+ |
+ | Without running the program, describe the elements of the stream defined by
+ |
+ | (define s (cons-stream 1 (add-streams s s)))
+ |
+ |
+ | Answer
+ |
+ |     1   2   4   8  16  32 ... = s
+ |     1   2   4   8  16  32 ... = s
+ | ─────────────────────────────────
+ | 1   2   4   8  16  32  64 ... = s
+ |
+ |#
+
+(define s (cons-stream 1 (add-streams s s)))
+(stream-take s 32)
