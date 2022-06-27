d4b1383e3b92f2b9fe0d269463d65466b1188848
SteeleDynamics <SteeleDynamics@gmail.com>
2022-06-27 19:21:18 -0400

    SICP Chapter 3 (In Progress...)
    1. Corrected exercise 3.60 (insert placeholder for lower-order term)

--- ch3/ex3.60/ex3.60.scm
+++ ch3/ex3.60/ex3.60.scm
@@ -250,22 +250,15 @@
 
 ; mul-series procedure
 (define (mul-series s1 s2)
-  (cons-stream (* (stream-car s1) (stream-car s2))
-               (add-streams
-                (add-streams
-                 (scale-stream (stream-cdr s2)
-                               (stream-car s1))
-                 (scale-stream (stream-cdr s1)
-                               (stream-car s2)))
-                (mul-series (stream-cdr s1)
-                            (stream-cdr s2)))))
+  (cons-stream
+    (* (stream-car s1) (stream-car s2))
+    (add-streams (add-streams (scale-stream (stream-cdr s2) (stream-car s1))
+                              (scale-stream (stream-cdr s1) (stream-car s2)))
+                (cons-stream 0 (mul-series (stream-cdr s1) (stream-cdr s2))))))
 
 ; cos²x + sin²x = 1
 (stream-take
- (scale-stream
-  (partial-sums
-   (add-streams
+  (add-streams
     (mul-series cosine-series cosine-series)
-    (mul-series sine-series sine-series)))
-  1.0)
- 20)
+    (mul-series sine-series sine-series))
+  8)
