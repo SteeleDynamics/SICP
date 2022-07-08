386afa76b4d99e6994a5235b001476cb9fda37fd
SteeleDynamics <SteeleDynamics@gmail.com>
2022-07-08 13:23:46 -0400

    SICP Chapter 3 (In Progress...)
    1. Corrected exercise 3.76 (Updated problem statement)

--- ch3/ex3.76/ex3.76.scm
+++ ch3/ex3.76/ex3.76.scm
@@ -360,27 +360,16 @@
   (lambda (x) (+ (* a (expt -1 (integer-floor (* 2 (- x b)) c))) d)))
 
 #|
- | Exercise 3.75
+ | Exercise 3.76
  |
- | Unfortunately, Alyssa's zero-crossing detector in exercise 3.74 proves to
- | be insufficient, because the noisy signal from the sensor leads to spurious
- | zero crossings. Lem E. Tweakit, a hardware specialist, suggests that Alyssa
- | smooth the signal to filter out the noise before extracting the zero
- | crossings. Alyssa takes his advice and decides to extract the zero crossings
- | from the signal constructed by averaging each value of the sense data with
- | the previous value. She explains the problem to her assistant, Louis
- | Reasoner, who attempts to implement the idea, altering Alyssa's program as
- | follows:
- |
- | (define (make-zero-crossings input-stream last-value)
- |   (let ((avpt (/ (+ (stream-car input-stream) last-value) 2)))
- |     (cons-stream (sign-change-detector avpt last-value)
- |                  (make-zero-crossings (stream-cdr input-stream)
- |                                       avpt))))
- |
- | This does not correctly implement Alyssa's plan. Find the bug that Louis has
- | installed and fix it without changing the structure of the program. (Hint:
- | You will need to increase the number of arguments to make-zero-crossings.)
+ | Eva Lu Ator has a criticism of Louis's approach in exercise 3.75. The
+ | program he wrote is not modular, because it intermixes the operation of
+ | smoothing with the zero-crossing extraction. For example, the extractor
+ | should not have to be changed if Alyssa finds a better way to condition her
+ | input signal. Help Louis by writing a procedure smooth that takes a stream
+ | as input and produces a stream in which each element is the average of two
+ | successive input stream elements. Then use smooth as a component to
+ | implement the zero-crossing detector in a more modular style.
  |#
 
 ; random-in-range procedure (from exercise 3.5)
