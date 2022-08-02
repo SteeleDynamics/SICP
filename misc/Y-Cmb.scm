c8bfda856b0e1486a7cba6ec48816d2cd512a5f7
SteeleDynamics <SteeleDynamics@gmail.com>
2022-08-02 08:37:16 -0400

    Add Y-Combinator example and output

---
+++ misc/Y-Cmb.scm
@@ -0,0 +1,16 @@
+; Y-Combinator procedure
+(define Y
+  (lambda (f)
+    ((lambda (x) (f (lambda (a) ((x x) a))))
+     (lambda (x) (f (lambda (a) ((x x) a)))))))
+
+; F procedure whose fixed-point solution is fact
+(define F
+  (lambda (g)
+    (lambda (x)
+      (if (< x 2)
+          x
+          (* x (g (- x 1)))))))
+
+; ((Y F) 6) ==> 720
+((Y F) 6)
