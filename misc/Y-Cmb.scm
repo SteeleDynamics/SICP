bd9d171963bc457bc54c585c8fdc6d01b42a17f8
SteeleDynamics <SteeleDynamics@gmail.com>
2024-07-17 12:06:59 -0400

    Updated Y-Cmb example

--- misc/Y-Cmb.scm
+++ misc/Y-Cmb.scm
@@ -1,10 +1,10 @@
-; Y-Combinator procedure
+;; Y-Combinator procedure
 (define Y
   (lambda (f)
     ((lambda (x) (f (lambda (a) ((x x) a))))
      (lambda (x) (f (lambda (a) ((x x) a)))))))
 
-; F procedure whose fixed-point solution is fact
+;; F procedure whose fixed-point solution is fact
 (define F
   (lambda (g)
     (lambda (x)
@@ -12,5 +12,5 @@
           x
           (* x (g (- x 1)))))))
 
-; ((Y F) 6) ==> 720
+;; ((Y F) 6) ==> 720
 ((Y F) 6)
